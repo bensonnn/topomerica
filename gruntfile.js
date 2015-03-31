6c6090eded74fa23b77898455fbca3bf94e1ea32
Dan Benson <dan.s.benson@gmail.com>
2015-03-31 15:04:19 -0700

    sass support

--- gruntfile.js
+++ gruntfile.js
@@ -5,59 +5,77 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-      connect: {
-        server: {
-          options: {
-            base: 'src',
-            livereload: LIVERELOAD_PORT,
-            open: {
-              target: 'http://localhost:8000'
-            }
+
+    connect: {
+      server: {
+        options: {
+          base: 'src',
+          livereload: LIVERELOAD_PORT,
+          open: {
+            target: 'http://localhost:8000'
           }
-        },
-      },
-      watch: {
-        livereload: {
-          options: {
-            livereload: LIVERELOAD_PORT
-          },
-          files: [
-            'src/*.js',
-            'src/*.html',
-          ],
-          configFiles: {
-            files: [ 'Gruntfile.js']
-          }
-        },
-      },
-      bowerInstall: {
-
-        target: {
-
-          src: [
-            'src/index.html'  // .html support...
-          ],
-
-          // Optional:
-          // ---------
-          cwd: '',
-          dependencies: true,
-          devDependencies: false,
-          exclude: [],
-          fileTypes: {},
-          ignorePath: '',
-          overrides: {}
         }
       },
-      shell: {                            
-        install: {                  
-          options: {                  
-              stderr: false
-          },
-          command: function(package) {
-            return 'bower install ' + package + ' --save'
-          } 
+    },
+
+    watch: {
+      compass: {
+        // We watch and compile sass files as normal but don't live reload here
+        files: 'src/css/*.{scss,sass}',
+        tasks: ['compass:dev'],
+      },
+      livereload: {
+        files: ['src/*.js',
+                'src/*.html',
+                'src/css/*.css'],
+        options: {
+          livereload: LIVERELOAD_PORT
+        },
+      },
+      configFiles: {
+        files: [ 'Gruntfile.js']
+      }
+    },
+
+    bowerInstall: {
+
+      target: {
+
+        src: [
+          'src/index.html'  // .html support...
+        ],
+
+        // Optional:
+        // ---------
+        cwd: '',
+        dependencies: true,
+        devDependencies: false,
+        exclude: [],
+        fileTypes: {},
+        ignorePath: '',
+        overrides: {}
+      }
+    },
+
+    shell: {
+      install: {
+        options: {
+            stderr: false
+        },
+        command: function(package) {
+          return 'bower install ' + package + ' --save'
         }
+      }
+    },
+
+    compass: {
+      dev: {
+        options: {
+          sassDir: 'src/css',
+          cssDir: 'src/css',
+          debugInfo: true,
+        }
+      }
     }
   });
 
@@ -65,6 +83,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-bower-install');
   grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-contrib-compass');
 
   // Default task(s).
   grunt.registerTask('default', ['connect', 'watch']);
