29bb0a0f21bc6370af631346bd8051cb01b6938d
Dan Benson <dan.s.benson@gmail.com>
2015-05-12 18:05:33 -0700

    topo stuff

--- src/app.js
+++ src/app.js
@@ -1,4 +1,21 @@
 (function() {
+  console.log($);
+    var map = $('#container').datamaps({
+      scope: 'usa',
+      fills : {
+        defaultFill: '#E1E1E1'
+      },
+      done : attachClickEvent
+    });
 
 
+    function attachClickEvent(datamap) {
+      datamap.svg.selectAll('.datamaps-subunit').on('click', function(geography,d) {
+        var clicked = d3.select(this)
+        clicked.style('fill', 'gray')
+
+
+        console.log(this);
+      });
+    }
 })();
